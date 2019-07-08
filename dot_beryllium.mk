09c11e60f9b8a6bbd6291acd0a3dbd3728e66ed3
dlasdl <dineshdlas@live.com>
2019-07-08 07:00:14 +0000

    dot bring up && intial clean
    Signed-off-by: dlasdl <dineshdlas@live.com>

--- dot_beryllium.mk
+++ dot_beryllium.mk
@@ -6,16 +6,16 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common EvoX-ify stuff.
+# Inherit some common dot stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_BOOT_ANIMATION_RES_EVO := true
 TARGET_GAPPS_ARCH := arm64
-CUSTOM_BUILD_TYPE := OFFICIAL
+CUSTOM_BUILD_TYPE := UNOFFICIAL
 TARGET_INCLUDE_STOCK_ARCORE := true
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_beryllium
+PRODUCT_NAME := dot_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
